4e80770a64c030c8f26931ff8f18ec6905fe2912
Michael Brumlow <mbrumlow@gmail.com>
2016-08-03 21:05:29 -0500

    Added a bunch of test and moved code around so it could be testable.

--- ppm.go
+++ ppm.go
@@ -16,36 +16,23 @@ func decode(r *bufio.Reader) (image.Image, error) {
 
 	// Check magic.
 	if err := decodeMagic(r); err != nil {
-		return nil, fmt.Errorf("Failed to decode magic: %v", err.Error())
+		return nil, fmt.Errorf("Failed to decode magic: %v", err)
 	}
 
 	// Read comments and white space.
-	for {
-		if c, err := r.ReadByte(); err != nil {
-			return nil, fmt.Errorf("Failed to read comments: %v", err.Error())
-		} else if c == '#' {
-			if _, err := r.ReadBytes('\n'); err != nil {
-				return nil, fmt.Errorf("Failed to read white space: %v", err.Error())
-			}
-		} else if c != '\n' {
-			break
-		}
+	if err := decodeComments(r); err != nil {
+		return nil, fmt.Errorf("Failed to decode comments: %v", err)
 	}
 
 	// Get the image size.
-	var width, height int
-	if n, err := fmt.Fscanf(r, "%d %d\n", &width, &height); err != nil {
-		return nil, fmt.Errorf("Failed to read size and format: %v", err.Error())
-	} else if n != 2 {
-		return nil, fmt.Errorf("Failed to read image size.")
+	width, height, err := decodeWidthHeight(r)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode width/height: %v", err)
 	}
 
 	// Get image color format.
-	var rgb_color int
-	if n, err := fmt.Fscanf(r, "%d\n", &rgb_color); err != nil {
-		return nil, fmt.Errorf("Failed to read color format: %v", err.Error())
-	} else if n != 1 {
-		return nil, fmt.Errorf("Failed to read color format.")
+	if _, err := decodeFormat(r); err != nil {
+		return nil, fmt.Errorf("Failed to decode format; %v", err)
 	}
 
 	// Read the image data.
@@ -69,18 +56,65 @@ func decode(r *bufio.Reader) (image.Image, error) {
 
 func decodeMagic(r *bufio.Reader) error {
 
-	if c, err := r.ReadByte(); err != nil {
-		return fmt.Errorf("Failed to read header: %v", err.Error())
-	} else if c != 'P' {
-		return fmt.Errorf("Invalid magic; expected 'P'")
+	s, err := r.ReadString('\n')
+	if err != nil {
+		return fmt.Errorf("Failed to read magic: %v", err.Error())
 	}
 
-	if c, err := r.ReadByte(); err != nil {
-		return fmt.Errorf("Failed to read header: %v", err.Error())
-	} else if c != '6' {
-		return fmt.Errorf("Invalid magic; expected '6'")
+	if s != "P6\n" {
+		return fmt.Errorf("Invalid magic.")
 	}
 
 	return nil
 
+}
+
+func decodeComments(r *bufio.Reader) error {
+
+	for {
+		if b, err := r.Peek(1); err != nil {
+			return err
+		} else if b[0] == '#' {
+			if _, err := r.ReadBytes('\n'); err != nil {
+				return fmt.Errorf("Failed to read white space: %v", err.Error())
+			}
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
+
+func decodeWidthHeight(r *bufio.Reader) (int, int, error) {
+
+	var width, height int
+	if n, err := fmt.Fscanf(r, "%d %d\n", &width, &height); err != nil {
+		return width, height, fmt.Errorf("Failed to read width/height: %v", err.Error())
+	} else if n != 2 {
+		return width, height, fmt.Errorf("Failed to read image width/height.")
+	}
+
+	if width < 0 || height < 0 {
+		return 0, 0, fmt.Errorf("out of range.")
+	}
+
+	return width, height, nil
+
+}
+
+func decodeFormat(r *bufio.Reader) (int, error) {
+
+	var rgb_color int
+	if n, err := fmt.Fscanf(r, "%d\n", &rgb_color); err != nil {
+		return rgb_color, fmt.Errorf("Failed to read format: %v", err.Error())
+	} else if n != 1 {
+		return rgb_color, fmt.Errorf("Failed to read format.")
+	}
+
+	if rgb_color != 255 {
+		return rgb_color, fmt.Errorf("Invalid or unsupported color format.")
+	}
+
+	return rgb_color, nil
 }
--- ppm_test.go
+++ ppm_test.go
@@ -22,7 +22,7 @@ func TestDecodeNoComments(t *testing.T) {
 }
 
 // Test PPM image decoding with comments in the header.
-func TestDecodeComments(t *testing.T) {
+func TestDecodeWithComments(t *testing.T) {
 
 	file, err := os.Open("images/im2.ppm")
 	if err != nil {
@@ -35,28 +35,125 @@ func TestDecodeComments(t *testing.T) {
 
 }
 
+// Test reading the magic.
 func TestDecodeMagic(t *testing.T) {
 
 	var magictest = []struct {
-		in    []byte
+		in    string
 		valid bool
 	}{
-		{[]byte{'P', '6'}, true},
-		{[]byte{' ', ' '}, false},
-		{[]byte{'6', 'P'}, false},
-		{[]byte{'p', '6'}, false},
-		{[]byte{'#', 'L'}, false},
-		{[]byte{}, false},
+		{"P6\n", true},
+		{"P6", false},
+		{"P6 ", false},
+		{"  ", false},
+		{"6P", false},
+		{"p6", false},
+		{"#L", false},
+		{"", false},
 	}
 
 	for _, tt := range magictest {
-		bb := bytes.NewBuffer(tt.in)
+
+		bb := bytes.NewBuffer([]byte(tt.in))
 		r := bufio.NewReader(bb)
 
 		if err := decodeMagic(r); tt.valid && err != nil {
-			t.Errorf("Failed to decode valid magic.")
+			t.Errorf("Failed to decode valid magic: %v", err)
 		} else if !tt.valid && err == nil {
 			t.Errorf("Decoded bad magic.")
+		}
+
+	}
+
+}
+
+// Tesst reading comments.
+func TestDecodeComments(t *testing.T) {
+
+	var commenttest = []struct {
+		in    string
+		valid bool
+	}{
+		{"", false},
+		{"#\n500 500", true},
+		{"#\n# comment\n500 500", true},
+		{"# comment\n500 500", true},
+		{"# comment 1\n#comment 2\n500 500", true},
+	}
+
+	for _, tt := range commenttest {
+
+		bb := bytes.NewBuffer([]byte(tt.in))
+		r := bufio.NewReader(bb)
+
+		if err := decodeComments(r); tt.valid && err != nil {
+			t.Errorf("Failed to decode comments: %v", err)
+		} else if !tt.valid && err == nil {
+			t.Errorf("Decoded bad comments.")
+		}
+	}
+
+}
+
+// Test reading width and height.
+func TestDecodeWidthHeight(t *testing.T) {
+
+	var widthheighttest = []struct {
+		in     string
+		width  int
+		height int
+		valid  bool
+	}{
+		{"", 0, 0, false},
+		{"\n", 0, 0, false},
+		{"800 600\n", 800, 600, true},
+		{"1000 1000\n", 1000, 1000, true},
+		{"-1 1000\n", -1, 1000, false},
+		{"-1 -1\n", -1, -1, false},
+	}
+
+	for _, tt := range widthheighttest {
+
+		bb := bytes.NewBuffer([]byte(tt.in))
+		r := bufio.NewReader(bb)
+
+		if width, height, err := decodeWidthHeight(r); tt.valid && err != nil {
+			t.Errorf("Failed to decode valid width/height: %v\n", err)
+		} else if !tt.valid && err == nil {
+			t.Errorf("Decoded bad width/height.")
+		} else if tt.valid && (width != tt.width || height != tt.height) {
+			t.Errorf("Wrong width/height.")
+		}
+	}
+
+}
+
+// Test reading format.
+func TestDecodeFormat(t *testing.T) {
+
+	var formattest = []struct {
+		in     string
+		format int
+		valid  bool
+	}{
+		{"255\n", 255, true},
+		{"100\n", -1, false},
+		{"-1\n", -1, false},
+		{"", 0, false},
+		{"\n", 0, false},
+	}
+
+	for i, tt := range formattest {
+
+		bb := bytes.NewBuffer([]byte(tt.in))
+		r := bufio.NewReader(bb)
+
+		if format, err := decodeFormat(r); tt.valid && err != nil {
+			t.Errorf("Failed to decode valid format: %v\n", err)
+		} else if !tt.valid && err == nil {
+			t.Errorf("Decoded bad format: %d.", i)
+		} else if tt.valid && format != tt.format {
+			t.Errorf("Wrong format.")
 		}
 
 	}
